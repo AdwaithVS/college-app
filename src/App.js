170aed5b30f66d3d5a90884629eb473b187ae2f4
AdwaithVS <adwaithvs512@gmail.com>
2024-05-11 18:24:26 +0530

    College app

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import HomePage from './components/HomePage';
 import GalleryPage from './components/GalleryPage';
 import AboutPage from './components/AboutPage';
 import ContactPage from './components/ContactPage';
+import TitleBar from './components/TitleBar';
 
 function App() {
   return (
     <div>
+      <TitleBar/>
       <HomePage/>
       <AboutPage/>
       <GalleryPage/>
--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -7,7 +7,6 @@ const AboutPage = () => {
                 <div className="row">
                     <h1>About Us</h1>
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <h2>Sree Buddha College of Engineering</h2>
                         <h3>Vision</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, aperiam aspernatur, incidunt in fugiat magnam consectetur rem provident harum necessitatibus fugit sequi itaque velit? Culpa dignissimos velit cupiditate deleniti quam.
                         Sequi, dignissimos quas? Voluptas quos vitae incidunt eum suscipit veniam commodi velit voluptate nemo molestias error, temporibus labore, voluptatem porro illo, quod hic asperiores quas debitis aperiam corporis pariatur inventore!
