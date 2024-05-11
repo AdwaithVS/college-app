6d4b5613f1f839a33a3abefe46d63c63ce8f1d16
AdwaithVS <adwaithvs512@gmail.com>
2024-05-11 18:13:33 +0530

    College app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './components/HomePage';
 import GalleryPage from './components/GalleryPage';
 import AboutPage from './components/AboutPage';
+import ContactPage from './components/ContactPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <HomePage/>
       <AboutPage/>
       <GalleryPage/>
+      <ContactPage/>
     </div>
   );
 }
