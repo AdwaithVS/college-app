9aa33f32a32b31648c286c2278a332caa45c799a
AdwaithVS <adwaithvs512@gmail.com>
2024-05-11 18:09:30 +0530

    College app

---
+++ src/components/GalleryPage.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const GalleryPage = () => {
+  return (
+    <div>
+      <div className="container">
+                <div className="row">
+                    <h1>Gallery</h1>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCP_AyX4glKv1fMJLrMBOneY8ljz1Ni_BEbPYDzobj-Q&s" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBs4rBFxkvkCh_2yj4HU8Sjk5mHb4mQwXCWi4L4_6hAg&s" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzauBaWdwsv55eejvJJUoYbzIw3-hILaBLp7JrXXE7DA&s" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwWMP-u1QdH83V3PPgiU23YgRJSUHWxPHV3xAoDlAQoA&s" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx-N9wK3-fTyOIN0DvhLaJ-JiGn1rGEIqDG70RDLp0Xg&s" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWRuwLmPbACngkqQ6gctEBzXtwVW4-YLlujUx3t__HMA&s" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyRVxpYKU5FWy17fhGnwCvdokA91vAYOb35cq51-T6hQ&s" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9xFG-rkoXTglpiQNxsxWq756Wq1Ifq8oNzygHsjz1ww&s" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    </div>
+  )
+}
+
+export default GalleryPage
