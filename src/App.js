f7e823c9bcd0df6a823c79bf0aa4bfcddb56f1f9
AdwaithVS <adwaithvs512@gmail.com>
2024-05-11 18:11:17 +0530

    College app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
 import GalleryPage from './components/GalleryPage';
+import AboutPage from './components/AboutPage';
 
 function App() {
   return (
     <div>
       <HomePage/>
+      <AboutPage/>
       <GalleryPage/>
     </div>
   );
---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <div className="container">
+                <div className="row">
+                    <h1>About Us</h1>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <h2>Sree Buddha College of Engineering</h2>
+                        <h3>Vision</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, aperiam aspernatur, incidunt in fugiat magnam consectetur rem provident harum necessitatibus fugit sequi itaque velit? Culpa dignissimos velit cupiditate deleniti quam.
+                        Sequi, dignissimos quas? Voluptas quos vitae incidunt eum suscipit veniam commodi velit voluptate nemo molestias error, temporibus labore, voluptatem porro illo, quod hic asperiores quas debitis aperiam corporis pariatur inventore!
+                        Nesciunt porro necessitatibus perferendis quis harum praesentium molestiae molestias corporis possimus reiciendis esse quaerat iste laudantium nam perspiciatis iusto, sint rerum at dolorem fugit quae? Neque iste veniam obcaecati quos!</p>
+                        <h3>Mission</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, facere repudiandae ea labore eum quisquam sint voluptatem sit iure odit adipisci nihil dolore magni dolores vero neque voluptatum laudantium error?
+                        Voluptatibus voluptas quasi quia rerum, cupiditate laborum enim, nemo sapiente quis hic, officia dignissimos nostrum. Et, atque modi magni eos eligendi velit, inventore, accusamus quam dignissimos libero tempore molestias voluptates?
+                        Ipsam assumenda sint saepe cum atque debitis asperiores minima, fuga, ipsa consequuntur deleniti eaque obcaecati perferendis doloribus aliquam tenetur? Ducimus ab soluta nihil sed illo ea fuga, beatae assumenda perspiciatis.</p>
+                    </div>
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default AboutPage
